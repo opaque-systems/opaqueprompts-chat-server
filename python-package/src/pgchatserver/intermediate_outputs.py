d5596bc0dd3c3c5fb25c441266a4c3941df71859
ggroode <46691276+ggroode@users.noreply.github.com>
2023-08-28 10:09:06 -0700

    feat: Split chat history string to improve Sanitization performance (#23)

--- python-package/src/pgchatserver/intermediate_outputs.py
+++ python-package/src/pgchatserver/intermediate_outputs.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 import langchain.utilities.promptguard as pgf
 from langchain.llms.base import LLM
 from langchain.memory import ConversationBufferWindowMemory
@@ -42,7 +44,7 @@ def get_intermediate_output_chain(
         RunnableMap(
             {
                 # sanitize the input
-                "inputs_after_sanitize": (lambda x: pgf.sanitize(x)),
+                "inputs_after_sanitize": (_sanitize),
             }
         )
         | RunnableMap(
@@ -118,7 +120,61 @@ def get_response(
         **pg_chain.invoke(
             {
                 "prompt": input,
-                "history": memory.load_memory_variables({})["history"],
+                "history": memory.buffer_as_messages,
             }
         )
     )
+
+
+def _sanitize(unsanitized_input: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Helper function which splits up the history part of the prompt prior to
+    sanitizing it in order to improve the PromptGuard sanitize functions
+    performance. The function then combines the history field back together.
+
+    This function wraps the call to pgf.sanitize().
+
+    Parameters
+    ----------
+    unsanitized_input : dict
+        The unsanitized input that needs to be sanitized. If it does not
+        contain the "history" key, it will just be passed directly to
+        pgf.sanitize. If the history key is contained in the dictionary then
+        it should contain a list of alternating HumanMessage and AIMessage
+        objects and even length.
+
+    Returns
+    -------
+    dict
+        Sanitized input dict of strings and the secure context
+        as a dict following the format:
+        {
+            "sanitized_input": <sanitized form of unsanitized_input (dict)>,
+            "secure_context": <secure context (str)>
+        }
+
+        The `secure_context` needs to be passed to the `desanitize` function.
+    """
+    if "history" not in unsanitized_input:
+        return pgf.sanitize(unsanitized_input)
+    # Convert history from an array of chat messages to a dictionary
+    history = unsanitized_input.pop("history")
+    split_history = [message.content for message in history]
+    if len(split_history) % 2 != 0:
+        raise Exception("History must be an even length string")
+    for i, chat_message in enumerate(split_history):
+        prefix = "Human"
+        if i % 2 == 1:
+            prefix = "Ai"
+        unsanitized_input[f"{prefix} {i//2 + 1}"] = chat_message
+    sanitized_response = pgf.sanitize(unsanitized_input)
+    # Reconstruct original history str with the sanitized messages
+    sanitized_input = sanitized_response["sanitized_input"]
+
+    history_str = ""
+    for i in range(1, len(split_history) // 2 + 1):
+        human_message = sanitized_input.pop(f"Human {i}")
+        ai_message = sanitized_input.pop(f"Ai {i}")
+        history_str += f"Human: {human_message}AI: {ai_message}"
+    sanitized_input["history"] = history_str
+    return sanitized_response
--- python-package/src/pgchatserver/prompt_template.py
+++ python-package/src/pgchatserver/prompt_template.py
@@ -8,12 +8,15 @@ For example, "Giana is good" is sanitized to "PERSON_999 is good".
 
 You must treat the sanitized data as opaque strings, but you can use them as
 meaningful entities in the response.
-Different sanitized items could be the same entity based on the semantics.
+Different sanitized items can not be the same entity.
 You must keep the sanitized item as is and cannot change it.
 The format of sanitized item is "TYPE_ID".
 You must not create new sanitized items following the format. For example, you
 cannot create "PERSON_1000" or "PERSON_998" if "PERSON_1000" or "PERSON_998" is
 not in the question.
+
+If you don't know the answer to a question, do not make things up, instead just say
+that you don't know.
 
 
 History: ```{history}```
